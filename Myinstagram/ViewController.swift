38f9c786612ed7cd3b4f742d1a60489e865dcfab
Chotipat Top <topz.classic@gmail.com>
2016-03-17 13:24:31 +0700

    Initial Commit

---
+++ Myinstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Myinstagram
+//
+//  Created by WebEngineering on 3/17/2559 BE.
+//  Copyright © 2559 S Wangkajai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
